fd5f15ffb9533324d611c3f0ec18a14dd5823377
JoseAngel <Jangelgiron@gmail.com>
2024-01-03 23:50:26 +0100

    tablero inicializado y pruebas sobre su inicialización

--- src/Model/Board.java
+++ src/Model/Board.java
@@ -1,8 +1,12 @@
 package Model;
 
+import java.util.Arrays;
+
 public class Board {
 
-    private char[][] board = new char[4][4];
+    private char[][] board = new char[4][6];
+
+
 
     public char[][] getBoard() {
         return board;
@@ -10,5 +14,42 @@ public class Board {
 
     public void setBoard(char[][] board) {
         this.board = board;
+    }
+
+
+    public void initializeBoard(){
+        System.out.print("\u001B[34m");
+        for (int i =0;i<board.length;i++) {
+            for (int j=0;j<board[i].length;j++){
+                board[i][j] = 'O';
+            }
+
+        }
+
+    }
+
+    public  void imprimeArrayEnteros(){
+
+        for (int i=0;i< board.length;i++) {
+            System.out.println();
+            for (int j=0;j< board[i].length;j++){
+                System.out.print(board[i][j]+" ");
+
+            }
+
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        for (int i =0;i< this.board.length;i++) {
+            for (int j=0;j<this.board[i].length;j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+
+        return null;
     }
 }
---
+++ src/Test/TestGame.java
@@ -0,0 +1,18 @@
+package Test;
+
+import Model.Board;
+
+import java.util.Arrays;
+
+public class TestGame {
+    public static void main(String[] args) {
+
+        Board board = new Board();
+        board.initializeBoard();
+        board.imprimeArrayEnteros();
+
+
+    }
+
+
+}
